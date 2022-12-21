f220d87f9af954433d3d53dc46edb0e27b1b69e7
Sorin Sagara <71135719+sorintolocica@users.noreply.github.com>
2022-12-21 15:15:04 +0200

    Update animeazu.js

--- animeazu.js
+++ animeazu.js
@@ -40,8 +40,11 @@ function displayPosts(response) {
       J = F.substring(5, 7),
       K = F.substring(8, 10),
       x = new Array;
-    x[1] = "Januari", x[2] = "Februari", x[3] = "Maret", x[4] = "April", x[5] = "May", x[6] = "Juni", x[7] = "Juli", x[8] = "Agustus", x[9] = "September", x[10] = "Oktober", x[11] = "November", x[12] = "Desember";
-
-    // Creați elementul li pentru postarea curentă
-    var li = document.createElement('li');
-    li.className = 'last-post-item';
+    x[1] = "Januari", x[2] = "Februari", x[3] = "Maret", x[4] = "April", x[5] = "May", x[6] = "Juni", x[7] = "Juli", x[8] = "Agustus", x[9] = "September", x[10] = "Oktober", x[11] = "November", x[12] = "Desember",
+      document.write('<li class="last-post-item">'), document.write('<div class="post_item"><a href="' + j + '" target ="_top">' + E + "</a></div>"), 1 == post_rilis && document.write('<span class="post-update">' + K + " " + x[parseInt(J, 10)] + " " + H + "</span>");
+    var G = "",
+      D = 0;
+    document.write("</li>")
+  }
+  document.write("</ul>")
+};
