e428f5bd039272725f15f9762b6c36f43edadfa4
Sorin Sagara <71135719+sorintolocica@users.noreply.github.com>
2022-12-21 15:14:07 +0200

    Update animeazu.js

--- animeazu.js
+++ animeazu.js
@@ -5,27 +5,36 @@
 // Setați numărul total de postări pe care doriți să le afișați
 var posts_no = 100;
 
-function post_latest(M) {
+// Obțineți feed-ul blogului prin apelarea API-ului Blogger
+var script = document.createElement('script');
+script.src = 'https://www.blogger.com/feeds/YOUR_BLOG_ID/posts/default?alt=json-in-script&callback=displayPosts';
+document.getElementsByTagName('head')[0].appendChild(script);
+
+function displayPosts(response) {
   // Creați un element ul pentru a afișa lista de postări
   var ul = document.createElement('ul');
   ul.className = 'last-post-wrap';
 
   // Parcurgeți fiecare postare în feed-ul blogului
   for (var I = 0; I < posts_no; I++) {
+    // Opriți bucla dacă am ajuns la numărul total de postări
+    if (I >= posts_no) {
+      break;
+    }
+
     // Obțineți detaliile postării curente
-    var z = M.feed.entry[I];
-    var E = z.title.$t;
-    var j;
+    var z = response.feed.entry[I],
+      E = z.title.$t;
+
+    // Căutați link-ul postării
     for (var y = 0; y < z.link.length; y++) {
-      if ("replies" == z.link[y].rel && "text/html" == z.link[y].type) {
-        var C = z.link[y].title,
-          B = z.link[y].href
-      }
       if ("alternate" == z.link[y].rel) {
-        j = z.link[y].href;
-        break
+        var j = z.link[y].href;
+        break;
       }
     }
+
+    // Obțineți data postării
     var F = z.published.$t,
       H = F.substring(0, 4),
       J = F.substring(5, 7),
@@ -33,21 +42,6 @@ function post_latest(M) {
       x = new Array;
     x[1] = "Januari", x[2] = "Februari", x[3] = "Maret", x[4] = "April", x[5] = "May", x[6] = "Juni", x[7] = "Juli", x[8] = "Agustus", x[9] = "September", x[10] = "Oktober", x[11] = "November", x[12] = "Desember";
 
-    // Creați un element li pentru fiecare postare
+    // Creați elementul li pentru postarea curentă
     var li = document.createElement('li');
     li.className = 'last-post-item';
-
-    // Creați un element div pentru a conține titlul postării
-    var div = document.createElement('div');
-    div.className = 'post_item';
-
-    // Creați un element a pentru titlul postării
-    var a = document.createElement('a');
-    a.href = j;
-    a.target = '_top';
-    a.textContent = E;
-
-    // Adăugați titlul postării în elementul div
-    div.appendChild(a);
-
-    // Adăugați elementul div în elementul li
